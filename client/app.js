a0809387f06c715a2ba108de6831f87dd654eb45
beiatrix <bpedrasa@gmail.com>
2019-10-15 19:57:23 -0700

    new ui

--- client/app.js
+++ client/app.js
@@ -1,14 +1,14 @@
 import React from 'react'
 
-import {Navbar} from './components'
-import Routes from './routes'
+// import {Navbar} from './components'
+// import Routes from './routes'
+import {Daily} from './components'
 
 const App = () => {
   return (
-    <div>
-      <Navbar />
-      <Routes />
-    </div>
+    // <Navbar />
+    // <Routes />
+    <Daily />
   )
 }
 
